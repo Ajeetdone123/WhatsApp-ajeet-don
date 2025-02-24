076cd0f3eb805ba7c347909787131251dbd53223
Ajeetdone123 <165822058+Ajeetdone123@users.noreply.github.com>
2025-02-24 21:25:36 +0530

    Update ajeet_don_whatsApp_loader.js

--- ajeet_don_whatsApp_loader.js
+++ ajeet_don_whatsApp_loader.js
@@ -36,7 +36,7 @@
       console.log(color("║  RULL3X     : TEDDY RULEX🧸 ", "31"));
       console.log(color("║  V3RSO1N  : WHATSSP 2.376", "34"));
       console.log(color("║  ONW3R      : MR AJEET L3G3ND", "36"));
-      console.log(color("║  GitHub       : "AHEET DON "35"));
+      console.log(color("║  GitHub       : "AJEET DON "35"));
       console.log(color("║  WH9TS9P  : +916387071869", "32"));
       console.log(color("╚═════════════════════════════════════════════════════════════╝", "33"));
     };
